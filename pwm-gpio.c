6fb6af857a06b1d03dd7b352f690f5cb038e25cf
pyavitz <pyavitz@gmail.com>
2022-10-18 10:01:08 -0400

    updated for usage with 5.15.y and up.
    currently failing against 6.x.y

--- pwm-gpio.c
+++ pwm-gpio.c
@@ -1,8 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) 2019 Angelo Compagnucci <angelo.compagnucci@gmail.com>
- *
- * pwm-gpio.c - driver for pwm output on gpio via high resolution timers.
+ * Copyright (c) 2015 Olliver Schinagl <oliver@schinagl.nl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +11,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ * This driver adds a high-resolution timer based PWM driver. Since this is a
+ * bit-banged driver, accuracy will always depend on a lot of factors, such as
+ * GPIO toggle speed and system load.
  */
 
 #include <linux/mod_devicetable.h>
@@ -31,50 +31,53 @@
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 
+#define DRV_NAME "pwm-gpio"
+
+struct gpio_pwm_data {
+	struct hrtimer timer;
+	struct gpio_desc *gpiod;
+	bool polarity;
+	bool pin_on;
+	int on_time;
+	int off_time;
+	bool run;
+};
+
 struct gpio_pwm_chip {
 	struct pwm_chip chip;
 };
 
-struct gpio_pwm_data {
-
-	bool is_running;
-	bool polarity;
-	bool pin_on;
-	unsigned int on_time;
-	unsigned int off_time;
-	struct mutex lock;
-	struct hrtimer timer;
-	struct gpio_desc *gpio_pwm;
-};
-
 static void gpio_pwm_off(struct gpio_pwm_data *gpio_data)
 {
-	gpiod_set_value_cansleep(gpio_data->gpio_pwm,
-		gpio_data->polarity ? 1 : 0);
-	gpio_data->pin_on = false;
+	gpiod_set_value_cansleep(gpio_data->gpiod, gpio_data->polarity ? 0 : 1);
 }
 
 static void gpio_pwm_on(struct gpio_pwm_data *gpio_data)
 {
-	gpiod_set_value_cansleep(gpio_data->gpio_pwm,
-		gpio_data->polarity ? 0 : 1);
-	gpio_data->pin_on = true;
+	gpiod_set_value_cansleep(gpio_data->gpiod, gpio_data->polarity ? 1 : 0);
 }
 
 enum hrtimer_restart gpio_pwm_timer(struct hrtimer *timer)
 {
 	struct gpio_pwm_data *gpio_data = container_of(timer,
-						  struct gpio_pwm_data,
-						  timer);
+						      struct gpio_pwm_data,
+						      timer);
+	if (!gpio_data->run) {
+		gpio_pwm_off(gpio_data);
+		gpio_data->pin_on = false;
+		return HRTIMER_NORESTART;
+	}
 
 	if (!gpio_data->pin_on) {
-		gpio_pwm_on(gpio_data);
 		hrtimer_forward_now(&gpio_data->timer,
 				    ns_to_ktime(gpio_data->on_time));
+		gpio_pwm_on(gpio_data);
+		gpio_data->pin_on = true;
 	} else {
-		gpio_pwm_off(gpio_data);
 		hrtimer_forward_now(&gpio_data->timer,
 				    ns_to_ktime(gpio_data->off_time));
+		gpio_pwm_off(gpio_data);
+		gpio_data->pin_on = false;
 	}
 
 	return HRTIMER_RESTART;
@@ -85,10 +88,8 @@ static int gpio_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 {
 	struct gpio_pwm_data *gpio_data = pwm_get_chip_data(pwm);
 
-	mutex_lock(&gpio_data->lock);
 	gpio_data->on_time = duty_ns;
 	gpio_data->off_time = period_ns - duty_ns;
-	mutex_unlock(&gpio_data->lock);
 
 	return 0;
 }
@@ -98,9 +99,7 @@ static int gpio_pwm_set_polarity(struct pwm_chip *chip, struct pwm_device *pwm,
 {
 	struct gpio_pwm_data *gpio_data = pwm_get_chip_data(pwm);
 
-	mutex_lock(&gpio_data->lock);
 	gpio_data->polarity = (polarity != PWM_POLARITY_NORMAL) ? true : false;
-	mutex_unlock(&gpio_data->lock);
 
 	return 0;
 }
@@ -109,18 +108,19 @@ static int gpio_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
 {
 	struct gpio_pwm_data *gpio_data = pwm_get_chip_data(pwm);
 
-	mutex_lock(&gpio_data->lock);
-
-	if (gpio_data->is_running) {
-		mutex_unlock(&gpio_data->lock);
+	if (gpio_data->run)
 		return -EBUSY;
+
+	gpio_data->run = true;
+	if (gpio_data->off_time) {
+		hrtimer_start(&gpio_data->timer, ktime_set(0, 0),
+			      HRTIMER_MODE_REL);
+	} else {
+		if (gpio_data->on_time)
+			gpio_pwm_on(gpio_data);
+		else
+			gpio_pwm_off(gpio_data);
 	}
-
-	gpio_data->is_running = true;
-	hrtimer_start(&gpio_data->timer, ktime_set(0, 0),
-			HRTIMER_MODE_REL);
-
-	mutex_unlock(&gpio_data->lock);
 
 	return 0;
 }
@@ -129,73 +129,29 @@ static void gpio_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm)
 {
 	struct gpio_pwm_data *gpio_data = pwm_get_chip_data(pwm);
 
-	mutex_lock(&gpio_data->lock);
-
-	if (!gpio_data->is_running) {
-		mutex_unlock(&gpio_data->lock);
-		return;
-	}
-
-	hrtimer_cancel(&gpio_data->timer);
-	gpio_pwm_off(gpio_data);
-	gpio_data->is_running = false;
-
-	mutex_unlock(&gpio_data->lock);
-}
-
-static int gpio_pwm_request(struct pwm_chip *chip, struct pwm_device *pwm)
-{
-	struct gpio_pwm_data *gpio_data;
-	struct gpio_desc *gpio_pwm;
-
-	gpio_data = kzalloc(sizeof(*gpio_data), GFP_KERNEL);
-	if (!gpio_data)
-		return -ENOMEM;
-
-	gpio_pwm = gpiod_get(chip->dev, "pwm", GPIOD_OUT_LOW);
-	if (IS_ERR(gpio_pwm)) {
-		dev_err(chip->dev, "failed to retrieve pwm from dts\n");
-		return PTR_ERR(gpio_pwm);
-	}
-
-	hrtimer_init(&gpio_data->timer,
-		     CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-
-	if (!hrtimer_is_hres_active(&gpio_data->timer))
-		dev_warn(chip->dev, "HR timer unavailable, restricting to \
-				     low resolution\n");
-
-	gpio_data->timer.function = &gpio_pwm_timer;
-	gpio_data->gpio_pwm = gpio_pwm;
-	gpio_data->pin_on = false;
-	gpio_data->is_running = false;
-
-	return pwm_set_chip_data(pwm, gpio_data);
-}
-
-static void gpio_pwm_free(struct pwm_chip *chip, struct pwm_device *pwm)
-{
-	struct gpio_pwm_data *gpio_data = pwm_get_chip_data(pwm);
-
-	gpio_pwm_disable(chip, pwm);
-	gpiod_put(gpio_data->gpio_pwm);
-	kfree(gpio_data);
+	gpio_data->run = false;
+	if (!gpio_data->off_time)
+		gpio_pwm_off(gpio_data);
 }
 
 static const struct pwm_ops gpio_pwm_ops = {
-	.config         = gpio_pwm_config,
-	.set_polarity   = gpio_pwm_set_polarity,
-	.enable         = gpio_pwm_enable,
-	.disable        = gpio_pwm_disable,
-	.request        = gpio_pwm_request,
-	.free           = gpio_pwm_free,
-	.owner          = THIS_MODULE,
+	.config = gpio_pwm_config,
+	.set_polarity = gpio_pwm_set_polarity,
+	.enable = gpio_pwm_enable,
+	.disable = gpio_pwm_disable,
+	.owner = THIS_MODULE,
 };
 
 static int gpio_pwm_probe(struct platform_device *pdev)
 {
-	struct gpio_pwm_chip *gpio_chip;
 	int ret;
+	struct gpio_pwm_chip *gpio_chip;
+	int npwm, i;
+	int hrtimer = 0;
+
+	npwm = of_gpio_named_count(pdev->dev.of_node, "pwm-gpios");
+	if (npwm < 1)
+		return -ENODEV;
 
 	gpio_chip = devm_kzalloc(&pdev->dev, sizeof(*gpio_chip), GFP_KERNEL);
 	if (!gpio_chip)
@@ -204,29 +160,83 @@ static int gpio_pwm_probe(struct platform_device *pdev)
 	gpio_chip->chip.dev = &pdev->dev;
 	gpio_chip->chip.ops = &gpio_pwm_ops;
 	gpio_chip->chip.base = -1;
-	gpio_chip->chip.npwm = 1;
+	gpio_chip->chip.npwm = npwm;
+	gpio_chip->chip.of_xlate = of_pwm_xlate_with_flags;
+	gpio_chip->chip.of_pwm_n_cells = 3;
 
 	ret = pwmchip_add(&gpio_chip->chip);
 	if (ret < 0) {
-		dev_err(&pdev->dev, "failed to add pwm gpio chip %d\n", ret);
-		return -ENODEV;
+		dev_err(&pdev->dev, "failed to add PWM chip: %d\n", ret);
+		return ret;
+	}
+
+	for (i = 0; i < npwm; i++) {
+		struct gpio_desc *gpiod;
+		struct gpio_pwm_data *gpio_data;
+
+		gpiod = devm_gpiod_get_index(&pdev->dev, "pwm", i,
+					     GPIOD_OUT_LOW);
+		if (IS_ERR(gpiod)) {
+			int error;
+
+			error = PTR_ERR(gpiod);
+			if (error != -EPROBE_DEFER)
+				dev_err(&pdev->dev,
+					"failed to get gpio flags, error: %d\n",
+					error);
+			return error;
+		}
+
+		gpio_data = devm_kzalloc(&pdev->dev, sizeof(*gpio_data),
+					 GFP_KERNEL);
+
+		hrtimer_init(&gpio_data->timer,
+			     CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+		gpio_data->timer.function = &gpio_pwm_timer;
+		gpio_data->gpiod = gpiod;
+		gpio_data->pin_on = false;
+		gpio_data->run = false;
+
+		if (hrtimer_is_hres_active(&gpio_data->timer))
+			hrtimer++;
+
+		pwm_set_chip_data(&gpio_chip->chip.pwms[i], gpio_data);
+	}
+	if (!hrtimer) {
+		dev_warn(&pdev->dev, "unable to use High-Resolution timer,");
+		dev_warn(&pdev->dev, "%s is restricted to low resolution.",
+			 DRV_NAME);
 	}
 
 	platform_set_drvdata(pdev, gpio_chip);
+
+	dev_info(&pdev->dev, "%d gpio pwms loaded\n", npwm);
 
 	return 0;
 }
 
 static int gpio_pwm_remove(struct platform_device *pdev)
 {
-	struct gpio_pwm_chip *gpio_chip = platform_get_drvdata(pdev);
+	struct gpio_pwm_chip *gpio_chip;
+	int i;
 
-	return pwmchip_remove(&gpio_chip->chip);
+	gpio_chip = platform_get_drvdata(pdev);
+	for (i = 0; i < gpio_chip->chip.npwm; i++) {
+		struct gpio_pwm_data *gpio_data;
+
+		gpio_data = pwm_get_chip_data(&gpio_chip->chip.pwms[i]);
+
+		hrtimer_cancel(&gpio_data->timer);
+	}
+
+	pwmchip_remove(&gpio_chip->chip);
+
+	return 0;
 }
 
 static const struct of_device_id gpio_pwm_of_match[] = {
-	{ .compatible = "pwm-gpio", },
-	{},
+	{ .compatible = DRV_NAME, },
+	{/* sentinel */},
 };
 MODULE_DEVICE_TABLE(of, gpio_pwm_of_match);
 
@@ -234,12 +244,12 @@ static struct platform_driver gpio_pwm_driver = {
 	.probe = gpio_pwm_probe,
 	.remove = gpio_pwm_remove,
 	.driver = {
-		.name = "pwm-gpio",
+		.name = DRV_NAME,
 		.of_match_table = of_match_ptr(gpio_pwm_of_match),
 	},
 };
 module_platform_driver(gpio_pwm_driver);
 
-MODULE_AUTHOR("Angelo Compagnucci <angelo.compagnucci@gmail.com>");
+MODULE_AUTHOR("Olliver Schinagl <oliver@schinagl.nl>");
 MODULE_DESCRIPTION("Generic GPIO bit-banged PWM driver");
 MODULE_LICENSE("GPL");
